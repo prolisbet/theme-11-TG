e47e908a309f5b22d30447978cf75dae5995fbea
prolisbet <lizatrushina96@gmail.com>
2024-09-13 00:43:52 +0300

    theme-11-TG

---
+++ main.py
@@ -0,0 +1,20 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer('Привет! Я бот!')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
