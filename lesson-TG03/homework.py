ded3f710493447e6957b4b4efe472296d5fc0cbb
prolisbet <lizatrushina96@gmail.com>
2024-09-15 22:20:48 +0300

    lesson TG03 bot

---
+++ lesson-TG03/homework.py
@@ -0,0 +1,86 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, FSInputFile
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from config import TOKEN
+import sqlite3
+import aiohttp
+import logging
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Form(StatesGroup):
+    name = State()
+    age = State()
+    grade = State()
+
+
+def init_db():
+    conn = sqlite3.connect('school_data.db')
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        grade TEXT NOT NULL
+    )
+    ''')
+    conn.commit()
+    conn.close()
+
+
+init_db()
+
+
+@dp.message(CommandStart())
+async def start(message: Message, state: FSMContext):
+    await message.answer('Привет! Как тебя зовут?')
+    await state.set_state(Form.name)
+
+
+@dp.message(Form.name)
+async def name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer('Сколько тебе лет?')
+    await state.set_state(Form.age)
+
+
+@dp.message(Form.age)
+async def age(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer('В каком классе ты учишься?')
+    await state.set_state(Form.grade)
+
+
+@dp.message(Form.grade)
+async def city(message: Message, state: FSMContext):
+    await state.update_data(grade=message.text)
+    student_data = await state.get_data()
+
+    conn = sqlite3.connect('school_data.db')
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO students (name, age, grade) 
+        VALUES (?, ?, ?)
+        ''', (student_data['name'], student_data['age'], student_data['grade']))
+    conn.commit()
+    conn.close()
+
+    await message.answer('Спасибо за информацию! Теперь ты в табличке.')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
