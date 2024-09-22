4b05843de92dcacbd16af5b897d8a5a60b88e573
prolisbet <lizatrushina96@gmail.com>
2024-09-23 00:14:12 +0300

    lesson-TG04 code

--- lesson-TG04/keyboards.py
+++ lesson-TG04/keyboards.py
@@ -9,6 +9,8 @@ main = ReplyKeyboardMarkup(keyboard=[
 
 inline_keyboard_test = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Видео', url='https://youtu.be/dQw4w9WgXcQ')],
+    [InlineKeyboardButton(text='Новости', callback_data='news')],
+    [InlineKeyboardButton(text='Профиль', callback_data='person')]
 ])
 
 test = ['Кнопка 1', 'Кнопка 2', 'Кнопка 3', 'Кнопка 4']
@@ -18,6 +20,13 @@ async def test_kb():
     keyboard = ReplyKeyboardBuilder()
     for key in test:
         keyboard.add(KeyboardButton(text=key))
+    return keyboard.adjust(2).as_markup(resize_keyboard=True)
+
+
+async def test_kb2():
+    keyboard = InlineKeyboardBuilder()
+    for key in test:
+        keyboard.add(InlineKeyboardButton(text=key, url='https://youtu.be/dQw4w9WgXcQ'))
     return keyboard.adjust(2).as_markup(resize_keyboard=True)
 
 
--- lesson-TG04/main4.py
+++ lesson-TG04/main4.py
@@ -1,7 +1,7 @@
 import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile, CallbackQuery
 from config import TOKEN, API_WEATHER_KEY
 import random
 import requests
@@ -12,9 +12,21 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
+@dp.callback_query(F.data == 'news')
+async def news(callback: CallbackQuery):
+    await callback.answer('Новости загружаются...')
+    # await callback.message.answer('Вот свежие новости!')
+    await callback.message.edit_text('Вот свежие новости!', reply_markup=await kb.test_kb2())
+
+
+@dp.message(F.text == 'Тестовая кнопка 1')
+async def test_button(message: Message):
+    await message.answer('Обработка нажатия на reply кнопку 1')
+
+
 @dp.message(CommandStart())
 async def start(message: Message):
-    await message.answer('Привет! Я твой бот на каждый день!', reply_markup=await kb.test_kb())
+    await message.answer('Привет! Я твой бот на каждый день!', reply_markup=kb.inline_keyboard_test)
 
 
 @dp.message(Command('help'))
